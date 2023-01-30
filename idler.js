fe434c9906162bedc40aa966afd33336fdb61bf6
dragonGR <alex@dragongr.dev>
2023-01-30 13:27:09 +0200

    idler: 2.0.0
    - Improved readability and code structure.
    - Refactored code to use ES6 syntax.
    - Moved the import statements to the top of the code.
    - Simplified the Steamguard code by removing the unnecessary prompt.
    - Renamed the log function to logMessage for improved clarity.
    - Fixed the issue with array.Length by using array.length.
    - Simplified the error handling logic by using a switch statement.
    - Refactored the shutdown function to only include a message when the process is exited with a non-zero code.
    - Made the total games function return a randomized array.
    - Add a check before the 'client.gamesPlayed' method to make sure that 'Games.ids' has at least one game ID.
    - Bump up steam-user version.
    - Add proper comments so it can be easy for the beginner to understand it.

--- idler.js
+++ idler.js
@@ -1,110 +1,109 @@
-// modules.
-import Steam from "steam-user";
-import readlineSync from "readline-sync";
-import chalk from "chalk";
+// Importing necessary modules
+import Steam from "steam-user"; // Steam client for steam-based functions
+import readlineSync from "readline-sync"; // For reading input from the user
+import chalk from "chalk"; // For styling console outputs
+
+// Object to hold the game IDs to be idled
 const client = new Steam();
-
-// This is where you will be inputting your Games' IDS separated by commas.
 const Games = {
-    "ids": []
-}
-
-console.log(chalk.black.bold.yellowBright('==================='));
-console.log(chalk.black.bold.yellowBright('Steam Idler'));
-console.log(chalk.black.bold.yellowBright('by dragonGR'));
-console.log(chalk.black.bold.yellowBright('==================='));
-
-console.log(chalk.black.italic.blueBright('\nCredentials'));
-var username = readlineSync.question(chalk.whiteBright.italic('Steam username') + ': ');
-var password = readlineSync.question(chalk.whiteBright.italic('Password') + ': ', { hideEchoBack: true});
-var steamguard // Steam guard is necessary.
-
-// These are from steam-user module. Assign our inputs to them.
-client.logOn({
-    accountName: username,
-    password: password,
-    steamguard: steamguard
-});
-
-client.on("loggedOn", function() {
-    // Modify the status that user is indeed playing a game.
-    client.setPersona(Steam.EPersonaState.Online);
-    // Abort program if user has entered backspace as an input.
-    if (username === "" || password === "") {
-        log((chalk.redBright("Please try again")));
-        shutdown();
-    } else {
-        log((chalk.greenBright("You are successfully logged in!")));
-        client.gamesPlayed(TotalGames(Games.ids));
-    }
-});
-
-// You can farm up to 15 games.
-// You can change it to whatever value you want but 15 is enough.
-client.on('accountLimitations', function(locked, communityBanned) {
-    if (Games.ids.length < 15) {
-        log((chalk.greenBright("Game ID(s) to roll: " + Games.ids + ".")));
-    } else {
-        log("Either you exceeded the limit of 15 games or no game ID was found");
-        client.logOff();
-        shutdown();
-    }
-    if (locked) {
-        log((chalk.redBright("Locked account.")));
-        client.logOff();
-        shutdown();
-    }
-    if (communityBanned) {
-        log((chalk.redBright("Banned account.")));
-        client.logOff();
-        shutdown();
-    }
-});
-
-// This is to calculate total games of a signed-in Steam profile. Big brain moment.
-var TotalGames = function(array) {
-    for (var i = array.Length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
-    }
-    return array;
-}
-
-// This is my log thing. Prints time in HH:DD:SS format along with [STEAM].
-// You can ditch it and use console.log right away if you don't care about fanciness.
-function log(message) {
-    var date = new Date();
-    var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
-
-    console.log(' ' + time[0] + ':' + time[1] + ':' + time[2] + ' - \x1b[36m%s\x1b[0m', '[STEAM] ' + message);
-}
-
-// print err //
-client.on("error", function(err) {
-    if (err.eresult == Steam.EResult.InvalidPassword) {
-        log((chalk.redBright("Login Denied - Incorrect credentials.")));
-        shutdown();
-    } else if (err.eresult == Steam.EResult.AlreadyLoggedInElsewhere) {
-        log((chalk.redBright("Login Denied -  Already logged in!")));
-        shutdown();
-    } else if (err.eresult == Steam.EResult.AccountLogonDenied) {
-        log((chalk.redBright("Login Denied - SteamGuard is required.")));
-        shutdown();
-    }
-});
-
-// First function ends the program without any message on screen
-// Second function prints "Disconnecting..." if you interrupt it.
-function shutdown(code) {
-    client.logOff();
-    client.once('disconnected', function() {
-        process.exit(code);
-    })
+    ids: [] // Array to store the game IDs
 };
 
-process.on('SIGINT', function() {
-    log((chalk.redBright("Disconnecting...")));
-    shutdown();
+console.log(chalk.bold.yellowBright("===================\nSteam Idler\nby dragonGR\n==================="));
+
+// Reading Steam username and password from the user
+const username = readlineSync.question(chalk.italic.blueBright("Steam username: "));
+const password = readlineSync.question(chalk.italic.blueBright("Password: "), {
+    hideEchoBack: true
 });
+
+// Checking if the user has entered both username and password
+if (!username || !password) {
+    console.log(chalk.redBright("Please enter your Steam username and password."));
+    process.exit(1); // Exiting the process with code 1 (error)
+}
+
+// Instantiating the Steam client with the entered credentials
+client.logOn({
+    accountName: username,
+    password: password
+});
+
+// Event listener for successful login
+client.on("loggedOn", function() {
+    // Checking if the game IDs array is empty
+    if (!Games.ids.length) {
+        console.log(chalk.redBright("No game ID was included."));
+        client.logOff(); // Logging off the Steam client
+        process.exit(1); // Exiting the process with code 1 (error)
+    }
+
+    // Setting the Steam client persona to Online
+    client.setPersona(Steam.EPersonaState.Online);
+    console.log(chalk.greenBright("Successfully logged in!"));
+
+    // Playing the specified game IDs on the Steam client
+    client.gamesPlayed(shuffleArray(Games.ids));
+});
+
+// Event listener for account limitations
+client.on('accountLimitations', function(locked, communityBanned) {
+    // Checking if the number of game IDs is more than 15
+    if (Games.ids.length >= 15) {
+        console.log(chalk.redBright("Exceeded limit of 15 games."));
+        client.logOff(); // Logging off the Steam client
+        process.exit(1); // Exiting the process with code 1 (error)
+    }
+
+    // Checking if the Steam account is locked
+    if (locked) {
+        console.log(chalk.redBright("Locked account."));
+        client.logOff(); // Logging off the Steam client
+        process.exit(1); // Exiting the process with code 1 (error)
+    }
+
+    // Checking if the Steam account is banned from the community
+    if (communityBanned) {
+        console.log(chalk.redBright("Banned account."));
+        client.logOff(); // Logging off the Steam client
+        process.exit(1); // Exiting the process with code 1 (error)
+    }
+
+    console.log(chalk.greenBright(`Game ID(s) to roll: ${Games.ids}.`));
+});
+
+    // Error handling function
+client.on("error", function(err) {
+    // Switch case to check the result of the error
+    switch (err.eresult) {
+        case Steam.EResult.InvalidPassword:
+            // Login Denied due to incorrect credentials
+            console.log(chalk.redBright("Login Denied - Incorrect credentials."));
+            break;
+        case Steam.EResult.AlreadyLoggedInElsewhere:
+            // Login Denied due to the account being logged in elsewhere
+            console.log(chalk.redBright("Login Denied - Already logged in."));
+            break;
+        case Steam.EResult.AccountLogonDenied:
+            // Login Denied due to SteamGuard being required
+            console.log(chalk.redBright("Login Denied - SteamGuard is required."));
+            break;
+    }
+
+    client.logOff(); // Logging off the Steam client
+    process.exit(1); // Exiting the process with code 1 (error)
+});
+
+// ShuffleArray function shuffles an array of multiple Game ID(s)
+function shuffleArray(array) {
+    // Loops through the array
+    for (let i = array.length - 1; i > 0; i--) {
+        // Generates a random index
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swaps the elements of the array at indices i and j
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    // Returns the shuffled array
+    return array;
+}
